69bcc08af4b48c1a914197f755e5a6715956be11
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2021-06-08 12:59:24 +0200

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -2,75 +2,73 @@ package Client;
 //Controlador. Classe que actua entre chatClient i socketClient
 import java.awt.event.*;
 
-public class ChatClient implements ActionListener{
+public class ChatClient implements ActionListener {
     Gui gui;
     SocketClient socketClient;
     String nick;
     
 
     //Constructor
-    public ChatClient(Gui client){
-        this.gui=client;
+    public ChatClient(Gui client) {
+        this.gui = client;
     }
 
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
     	String msg;
-        switch(e.getActionCommand()){
+        switch(e.getActionCommand()) {
             case "Enviar":
-                msg=this.gui.getMsg("Enviar");
+                msg = this.gui.getMsg("Enviar");
                 //this.gui.updateView(msg);
                 this.socketClient.sendMsg(nick+" -> "+msg);
                 this.gui.deleteMsg();
-            break;
+                break;
             case "Login":
-            	msg=this.gui.getMsg("Login");
+            	msg = this.gui.getMsg("Login");
             	System.out.println("Conectando con nick: "+msg);
             	if (msg != null) {
-            		nick=msg;
+            		nick = msg;
             		socketClient = new SocketClient();
             		socketClient.setControlador(this);
             		this.socketClient.connect();
         			this.socketClient.start();
             		
             		this.socketClient.sendMsg(msg);
-            	}else {
+            	} else {
             		this.gui.errorNick();
             	}
-            	
-            break;
+                break;
             case "Logout":
             	this.socketClient.sendMsg("QUIT:"+nick);
-            	
-            break;
+                break;
         }
     }
 
-    public void updateView(String msg){
+    public void updateView(String msg) {
     	//Control de posibles respuestas
     	String[] message = msg.split(":", 2);
     	switch(message[0]) {
     		case "ACCEPT":
-    			System.out.println("Connexió acceptada");
-    		break;
+    			System.out.println("ConnexiÃ³ acceptada");
+    		    break;
     		case "ERROR":
     			System.out.println("Error nick");
     			this.gui.errorNick();
-    		break;
+    		    break;
     		case "JOIN":
-    			System.out.println("Connexió establerta");
+    			System.out.println("ConnexiÃ³ establerta");
     			this.gui.connectEstablished();
-    		break;
+    		    break;
     		case "USERS":
     			System.out.println("Users:"+ message[1]);
     			this.gui.users(message[1]);
-    		break;
+    		    break;
     		case "MESSAGE":
     			this.gui.updateView(message[1]);
-    		break;
+    		    break;
     		case "QUIT":
     			this.socketClient.salir();
     			this.gui.salir();
-    		break;
+    		    break;
     	}
         
     }
