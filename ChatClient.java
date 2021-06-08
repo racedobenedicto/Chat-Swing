e70d110c7d4b15d31411354fd468828ff1039779
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2021-06-08 12:59:34 +0200

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -73,7 +73,7 @@ public class ChatClient implements ActionListener {
         
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
     	//final int port=5000;
         //final String host="localhost";
         Gui gui = new Gui(); //Vista
