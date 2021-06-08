c9b608a9329746c69a69a6d063e6873430964b60
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2021-06-08 13:04:43 +0200

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -3,63 +3,63 @@ package Client;
 import java.io.*;
 import java.net.*;
 
-public class SocketClient extends Thread{
+public class SocketClient extends Thread {
     ChatClient chat;
     int port;
     String host;
     Socket socket;
     BufferedReader bufferedReader;
     BufferedWriter bufferedWriter;
-    boolean active=true;
+    boolean active = true;
 
-    public SocketClient(){
-        this.port=5000;
-        this.host="localhost";
+    public SocketClient() {
+        this.port = 5000;
+        this.host = "localhost";
     }
 
     public void setControlador(ChatClient chat){
-        this.chat=chat;
+        this.chat = chat;
     }
 
-    public void connect(){
-        try{
-            this.socket=new Socket(host, port);
-            this.bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        }catch (IOException ex){
+    public void connect() {
+        try {
+            this.socket = new Socket(host, port);
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException ex) {
             System.out.println("Error connect");
         }
     }
 
-    public void sendMsg(String msg){
-        try{
+    public void sendMsg(String msg) {
+        try {
             bufferedWriter.write(msg);
             bufferedWriter.newLine();
             bufferedWriter.flush();
-        }catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("Error sendMsg");
         }
     }
 
-    public String readMsg(){
-        try{
-            String msg=bufferedReader.readLine();
+    public String readMsg() {
+        try {
+            String msg = bufferedReader.readLine();
             return msg;
-        }catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("Error readMsg");
         }
         return "";
     }
 
-    public void run(){
-        while(active){
-       		String msg=readMsg();
+    public void run() {
+        while(active) {
+       		String msg = readMsg();
             chat.updateView(msg);
         }
     }
     
     public void salir() {
-    	this.active=false;
+    	this.active = false;
     	try {
 			socket.close();
 		} catch (IOException e) {
@@ -72,8 +72,6 @@ public class SocketClient extends Thread{
     }
     
     public void setActive(boolean act) {
-    	this.active=act;
+    	this.active = act;
     }
-
 }
-
